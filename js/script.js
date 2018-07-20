435481ea6ce667989350587f86c1e02429eece70
Alex Moore <34859574+AleksMoore@users.noreply.github.com>
2018-07-20 16:03:03 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,3 +2,6 @@ window.onload = function() {
     alert( 'Добро пожаловать в "Чат". Приятного общения :)' );
 }
     
+window.onbeforeunload = function() {
+  return "Вы точно хотите покинуть чат ?!";
+};
